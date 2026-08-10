4e7c53cf0d78d203b114208da73c1cf99a8b7831
Chris Mangan <chrisjm66@gmail.com>
2026-08-10 16:13:05 -0400

    add prettier, eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import wails from "@wailsio/runtime/plugins/vite";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import wails from '@wailsio/runtime/plugins/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "127.0.0.1",
-    port: Number(process.env.WAILS_VITE_PORT) || 9245,
-    strictPort: true,
-  },
-  plugins: [svelte(), wails("./bindings")],
+	server: {
+		host: '127.0.0.1',
+		port: Number(process.env.WAILS_VITE_PORT) || 9245,
+		strictPort: true
+	},
+	plugins: [svelte(), wails('./bindings'), tailwindcss()]
 });
